3289e0e0da3caf0908ec059652863647db4579ed
tonyiovino <antonioiovino2004tony@gmail.com>
2021-01-29 00:31:38 +0100

    Cai Py

---
+++ cai/py/cai.py
@@ -0,0 +1,48 @@
+import random
+
+def generate_num(min, max):
+    return random.randint(min, max)
+
+def generate_string(risposta, risposta_esatta):
+
+    num = generate_num(1, 4)
+    num_esatte = 0
+
+    if risposta == risposta_esatta:
+        print_risposta_esatta(num)
+        num_esatte = num_esatte + 1
+
+    else:
+        print_risposta_errata(num)
+    
+    print('\n')
+
+    return num_esatte
+
+def print_risposta_esatta(num):
+    if num == 1:
+        print("Very good!")
+    elif num == 2:
+        print("Excellent!")
+    elif num == 3:
+        print("Nice work!")
+    elif num == 4:
+        print("Keep up the good work!")
+    else:
+        print("Error")
+
+def print_risposta_errata(num):
+    if num == 1:
+        print("No. Please try again.")
+    elif num == 2:
+        print("Wrong. Try once more.")
+    elif num == 3:
+        print("Don't give up!")
+    elif num == 4:
+        print("No. Keep trying.")
+    else:
+        print("Error")
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
---
+++ cai/py/main.py
@@ -0,0 +1,37 @@
+import cai
+
+def rispondi_e_controlla(a, b, risposta_esatta):
+
+    num_esatte = 0
+
+    print(f"Quanto fa {a} per {b}? (CTRL+C per uscire)")
+    risposta = int(input("Risposta: "))
+
+    num_esatte = num_esatte + cai.generate_string(risposta, risposta_esatta)
+
+    return num_esatte
+
+def controlla_risposte_esatte(num_esatte, num_domande):
+
+    percentuale = ( num_esatte / num_domande ) * 100
+
+    print(f"Your rate is: {percentuale}%")
+
+    if percentuale < 75: print("Please ask your instructor for extra help")
+
+    else: print("Good Job!")
+
+num_domande = 10
+num_risposte = 0
+num_esatte = 0
+
+while (num_risposte < num_domande):
+    a = cai.generate_num(0, 10)
+    b = cai.generate_num(0, 10)
+
+    risposta_esatta = a * b
+
+    num_esatte = num_esatte + rispondi_e_controlla(a, b, risposta_esatta)
+    num_risposte = num_risposte + 1
+
+controlla_risposte_esatte(num_esatte, num_domande)
