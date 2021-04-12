cd53862f792bae3d2d6a02ce5b9c7f458c79bea1
tonyiovino <antonioiovino2004tony@gmail.com>
2021-04-12 16:35:26 +0200

    Memoizzazione

---
+++ memoizzazione.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int fibonacci_helper(int n, int cache[]);
+int fibonacci(int n);
+
+int main(){
+
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		printf("%d ", fibonacci(i));
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
+int fibonacci(int n){
+
+	static int cache[100];
+
+	return fibonacci_helper(n, cache);
+}
+
+int fibonacci_helper(int n, int cache[]) {
+
+    if (n == 0 || n == 1) return n;
+
+	if (cache[n]){
+		return cache[n];
+	}
+
+	cache[n] = fibonacci_helper(n-1, cache) + fibonacci_helper(n-2, cache);
+
+    return cache[n];
+}
