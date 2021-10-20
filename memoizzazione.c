6753af3d0117a9bdf55dc907afcd647a43507fe5
tonyiovino <antonioiovino2004tony@gmail.com>
2021-10-20 19:24:28 +0200

    -m

--- memoizzazione.c
+++ memoizzazione.c
@@ -7,7 +7,7 @@ int main(){
 
 	int i;
 
-	for (i = 0; i < 5; i++) {
+	for (i = 0; i < 10; i++) {
 		printf("%d ", fibonacci(i));
 	}
 
@@ -18,23 +18,30 @@ int main(){
 
 int fibonacci(int n){
 
-	static int cache[47];
+	#define CACHE_SIZE 30
+	static int cache[CACHE_SIZE];
 
-	return fibonacci_helper(n, cache, 47);
+	cache[0] = 0;
+	cache[1] = 1;
+
+	if (n >= CACHE_SIZE) {
+		printf("fibonacci(): Il limite massimo è %d\n\n", CACHE_SIZE-1);
+		return -1;
+	}
+
+	return fibonacci_helper(n, cache, CACHE_SIZE);
 }
 
 int fibonacci_helper(int n, int cache[], int size_cache) {
 
 	if (n == 0 || n == 1) return n;
 
-	if (n > size_cache) {
-		printf("Il limite massimo è %d\n\n", size_cache);
-		return n;
+	if (n >= size_cache) {
+		printf("fibonacci_helper(): Il limite massimo è %d\n\n", size_cache-1);
+		return -1;
 	}
-	else if (cache[n]){
-		return cache[n];
-	}
-
+	
+	if (cache[n]) return cache[n];
 
 	cache[n] = fibonacci_helper(n-1, cache, size_cache) + fibonacci_helper(n-2, cache, size_cache);
 
